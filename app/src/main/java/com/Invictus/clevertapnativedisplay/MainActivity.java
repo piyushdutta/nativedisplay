76b0784e35a9da0ad4c781738c38cdd55805163a
piyush <piyush1234dutta@gmail.com>
2020-05-16 15:00:40 +0530

    intialized clevertap and acocunt credentials

--- app/src/main/java/com/Invictus/clevertapnativedisplay/MainActivity.java
+++ app/src/main/java/com/Invictus/clevertapnativedisplay/MainActivity.java
@@ -1,14 +1,28 @@
 package com.Invictus.clevertapnativedisplay;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.clevertap.android.sdk.CleverTapAPI;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CleverTapAPI cleverTapAPI = CleverTapAPI.getDefaultInstance(getApplicationContext());
+
+        cleverTapAPI.setDebugLevel(CleverTapAPI.LogLevel.DEBUG);
+
     }
+
 }
